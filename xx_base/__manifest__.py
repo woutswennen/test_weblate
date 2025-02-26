09bcc51079f464ecf72563b2eca684de5b6abc4e
wout <wout.swennen@dynapps.be>
2025-02-26 16:32:08 +0100

    [18.0] first module

---
+++ xx_base/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Customer Base",
+    "version": "18.0.1.0.0",
+    "author": "dynapps",
+    "website": "https://www.dynapps.eu",
+    "category": "Hidden",
+    "license": "LGPL-3",
+    "depends": [
+    ],
+    "data": [
+
+    ],
+    "installable": True,
+    "dyn_required": True,
+}
